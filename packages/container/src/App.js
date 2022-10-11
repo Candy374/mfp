e0524d2915f6f4c543c4fa777a480b985b3d82a7
ling huang <ling.huang@clearcapital.com>
2022-10-10 21:51:13 -0700

    add dash board

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,12 @@
 // must consist with useStyles, both should import from '@material-ui/core' or '@material-ui/core/styles'
 import {createGenerateClassName, StylesProvider} from '@material-ui/core'
-import React, {Suspense, useEffect, useState} from 'react'
+import React, {Suspense, useState} from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Header from './components/Header'
+import ProgressBar from './components/ProgressBar'
 const AuthApp = React.lazy(() => import('./components/AuthApp'))
 const MarketingApp = React.lazy(() => import('./components/MarketingApp'))
-import ProgressBar from './components/ProgressBar'
+const DashboardApp = React.lazy(() => import('./components/DashboardApp'))
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -23,6 +24,7 @@ export default () => {
             <Route path='/auth' component={AuthApp}>
               <AuthApp onSignIn={() => setIsSingedIn(true)} />
             </Route>
+            <Route path='/dashboard' component={DashboardApp} />
             <Route path='/' component={MarketingApp} />
           </Switch>
         </Suspense>
--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,24 +1,26 @@
 const {merge} = require('webpack-merge')
 const commonConfig = require('./webpack.common')
-
-const packageJson = require('../package.json')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
 const devConfig = {
   mode: 'development',
   devServer: {
-    port: 8080,
-    historyApiFallback: true
+    port: 8083,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
   },
   output: {
-    publicPath: 'http://localhost:8080/'
+    filename: '[name].[contenthash].js',
+    publicPath: 'http://localhost:8083/'
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-        auth: 'auth@http://localhost:8082/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js'
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap'
       },
       shared: packageJson.dependencies
     })
