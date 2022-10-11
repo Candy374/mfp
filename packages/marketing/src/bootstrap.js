837a87c37e946aff08ddcb173242f016e2abb573
ling huang <ling.huang@clearcapital.com>
2022-10-10 20:10:26 -0700

    add navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
+import {createMemoryHistory, createBrowserHistory} from 'history'
+const mount = (el, {onNavigate, defaultHistory}) => {
+  const history = defaultHistory || createMemoryHistory()
+  if (onNavigate) {
+    history.listen(onNavigate)
+  }
 
-const mount = el => {
-  ReactDom.render(<App />, el)
+  ReactDom.render(<App history={history} />, el)
+
+  return {
+    onParentNavigate({pathname: nextPathname}) {
+      if (history.location.pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.getElementById('_marketing_dev-root')
+
   if (devRoot) {
-    mount(devRoot)
+    mount(devRoot, {defaultHistory: createBrowserHistory()})
   }
 }
 
