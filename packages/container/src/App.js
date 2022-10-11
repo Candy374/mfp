11e909e1671400d7a48e6bfd33d5a7277069add2
ling huang <ling.huang@clearcapital.com>
2022-10-10 20:53:04 -0700

    update dev public path

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,10 @@
 // must consist with useStyles, both should import from '@material-ui/core' or '@material-ui/core/styles'
 import {createGenerateClassName, StylesProvider} from '@material-ui/core'
-import React from 'react'
+import React, {Suspense} from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Header from './components/Header'
-import MarketingApp from './components/MarketingApp'
-
 const AuthApp = React.lazy(() => import('./components/AuthApp'))
+const MarketingApp = React.lazy(() => import('./components/MarketingApp'))
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -15,13 +14,13 @@ export default () => {
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
-        <div>
-          <Header />
+        <Header />
+        <Suspense fallback={<div>loading</div>}>
           <Switch>
             <Route path='/auth' component={AuthApp} />
             <Route path='/' component={MarketingApp} />
           </Switch>
-        </div>
+        </Suspense>
       </StylesProvider>
     </BrowserRouter>
   )
